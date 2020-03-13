fa378743f719875ff8d23d8d01b76db1d23b70f4
Sergey Kozyr <thelichking9@gmail.com>
2020-03-13 11:07:02 +0300

    Updated functions' names, added exceptions

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import random
 from dotenv import load_dotenv
 
 
-def find_random_comic():
+def get_random_comic_url_and_comment():
   response = requests.get('http://xkcd.com/info.0.json')
   total_comic_number = response.json()['num']
   random_comic_number = random.randint(0, total_comic_number)
   response = requests.get(f'http://xkcd.com/{random_comic_number}/info.0.json')
-  comic_url = response.json()['img']
-  comic_comment = response.json()['alt']
+  comic_json_content = response.json()
+  comic_url = comic_json_content['img']
+  comic_comment = comic_json_content['alt']
   return comic_url, comic_comment
 
 
@@ -28,19 +29,23 @@ def upload_comic(access_token, group_id):
       "group_id": group_id
   }
   response = requests.get('https://api.vk.com/method/photos.getWallUploadServer', params=payload)
-  print(response.json())
+  if 'error' in response.json():
+    raise requests.exceptions.HTTPError(response.json()['error'])
   upload_url = response.json()['response']['upload_url']
-  with open('./image.png', 'rb') as comic:
+  with open('image.png', 'rb') as comic:
     files = {'photo': comic}
     response = requests.post(upload_url, files=files)
-    response.raise_for_status()
+    if 'error' in response.json():
+      raise requests.exceptions.HTTPError(response.json()['error'])
     payload.update({
         'hash': f'{response.json()["hash"]}',
         'photo': f'{response.json()["photo"]}',
         'server': f'{response.json()["server"]}'
     })
-    response_ = requests.post('https://api.vk.com/method/photos.saveWallPhoto', data=payload)
-    return response_.json()
+    response = requests.post('https://api.vk.com/method/photos.saveWallPhoto', data=payload)
+    if 'error' in response.json():
+      raise requests.exceptions.HTTPError(response.json()['error'])
+    return response.json()
 
 
 def post_comic(access_token, group_id, json_object, comic_comment):
@@ -52,17 +57,21 @@ def post_comic(access_token, group_id, json_object, comic_comment):
       "attachments": f"photo{json_object['response'][0]['owner_id']}_{json_object['response'][0]['id']}",
       "message": f'{comic_comment}'
   }
-  requests.get('https://api.vk.com/method/wall.post', params=payload)
+  response = requests.get('https://api.vk.com/method/wall.post', params=payload)
+  if 'error' in response.json():
+    raise requests.exceptions.HTTPError(response.json()['error'])
 
 
 def main():
   load_dotenv()
   access_token = os.getenv('ACCESS_TOKEN')
   group_id = os.getenv('GROUP_ID')
-  comic_url, comic_comment = find_random_comic()
+  comic_url, comic_comment = get_random_comic_url_and_comment()
   download_comic(comic_url)
-  post_comic(access_token, group_id, upload_comic(access_token, group_id), comic_comment)
-  os.remove('image.png')
+  try:
+    post_comic(access_token, group_id, upload_comic(access_token, group_id), comic_comment)
+  finally:
+    os.remove('image.png')
 
 
 if __name__ == "__main__":
